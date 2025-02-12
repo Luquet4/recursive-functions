84d46848a2f07325075865fb563c178f2f4cfc28
Luquet4 <lucasgabrielvmb@gmail.com>
2025-02-11 23:32:43 -0400

    Ex3 Conc.

---
+++ ex3.py
@@ -0,0 +1,21 @@
+'''
+Exercício 3: 
+
+Contar Caracteres em uma String
+Crie uma função recursiva chamada contar_caracteres(s, c) que conta quantas vezes o
+caractere c aparece na string s.
+
+Exemplo de Entrada:
+contar_caracteres("banana", "a")
+
+Saída Esperada:
+3
+'''
+
+def contar_caracteres(s, c):
+    if not s:
+        return 0
+    return (1 if s[0] == c else 0) + contar_caracteres(s[1:], c)
+
+print(contar_caracteres("banana", "a"))
+
